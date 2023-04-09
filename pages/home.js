4ccd560cd7e620dcd964c54bbcf07e782a79889e
Martin Šíl <martin.sil1337@gmail.com>
2023-04-10 00:30:15 +0200

    Přechod na RLS u zobrazování lekcí a detailu lekce

--- pages/home.js
+++ pages/home.js
@@ -26,33 +26,3 @@ export default function Home() {
 		</section>
 	);
 }
-
-// import { supabase } from '@/utils/supabase.js';
-// import Link from 'next/link';
-// import { useUzivatel } from '@/context/uzivatel';
-
-// export default function Home({ lekce }) {
-// 	const { uzivatel } = useUzivatel();
-// 	console.log({ uzivatel });
-// 	return (
-// 		<section>
-// 			{lekce.map((l) => (
-// 				<Link key={l.id} href={"/lekce/" + l.slug}>
-// 					<div>
-// 						<h1>{l.nazev}</h1>
-// 					</div>
-// 				</Link>
-// 			))}
-// 		</section>
-// 	);
-// }
-
-// export async function getServerSideProps() {
-// 	const { data: lekce } = await supabase.from('lekce').select('*');
-
-// 	return {
-// 		props: {
-// 			lekce,
-// 		},
-// 	};
-// }
--- pages/lekce/[slug].js
+++ pages/lekce/[slug].js
@@ -1,39 +1,94 @@
 import { supabase } from '@/utils/supabase';
 import MuxPlayer from '@mux/mux-player-react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
-export default function Lekce({ lekce }) {
+export default function Lekce({ slug }) {
+	const router = useRouter();
+	const [lekce, setLekce] = useState(null);
+
+	useEffect(() => {
+		const fetchLekceContent = async () => {
+			const { data: lekce, error } = await supabase
+				.from('lekce')
+				.select(`*`)
+				.eq('slug', slug)
+				.single();
+			if (!error) {
+				setLekce(lekce);
+			} else {
+				router.push('/lekce/error');
+			}
+		};
+
+		fetchLekceContent();
+	}, []);
+
 	return (
 		<>
-			<Head>
-				<title>{lekce.nazev}</title>
-			</Head>
-			<h1>{lekce.nazev}</h1>
-			<div style={{ width: '40vw', height: 'auto' }}>
-				<MuxPlayer streamType="on-demand" playbackId={lekce.playback_id} />
-			</div>
-			<div dangerouslySetInnerHTML={{ __html: lekce.obsah }} />
+			{lekce ? (
+				<>
+					<Head>
+						<title>{lekce.nazev}</title>
+					</Head>
+					<h1>{lekce.nazev}</h1>
+					<div style={{ width: '40vw', height: 'auto' }}>
+						<MuxPlayer streamType="on-demand" playbackId={lekce.playback_id} />
+					</div>
+					<div dangerouslySetInnerHTML={{ __html: lekce.obsah }} />
+				</>
+			) : (
+				<p>Načítá se</p>
+			)}
 		</>
 	);
 }
 
 export async function getServerSideProps({ params }) {
-	const { data: lekce } = await supabase
-		.from('lekce')
-		.select(`*`)
-		.eq('slug', params.slug)
-		.single();
-
-	if (!lekce || lekce.length === 0) {
-		return {
-			redirect: {
-				destination: '/lekce/neexistuje',
-				permanent: false,
-			},
-		};
-	}
+	const slug = params.slug;
 
 	return {
-		props: { lekce },
+		props: { slug },
 	};
 }
+
+// import { supabase } from '@/utils/supabase';
+// import MuxPlayer from '@mux/mux-player-react';
+// import Head from 'next/head';
+
+// export default function Lekce({ lekce }) {
+// 	return (
+// 		<>
+// 			<Head>
+// 				<title>{lekce.nazev}</title>
+// 			</Head>
+// 			<h1>{lekce.nazev}</h1>
+// 			<div style={{ width: '40vw', height: 'auto' }}>
+// 				<MuxPlayer streamType="on-demand" playbackId={lekce.playback_id} />
+// 			</div>
+// 			<div dangerouslySetInnerHTML={{ __html: lekce.obsah }} />
+// 		</>
+// 	);
+// }
+
+// export async function getServerSideProps({ params }) {
+// 	const { data: lekce } = await supabase
+// 		.from('lekce')
+// 		.select(`*`)
+// 		.eq('slug', params.slug)
+// 		.single();
+
+// 	if (!lekce || lekce.length === 0) {
+// 		return {
+// 			redirect: {
+// 				destination: '/lekce/neexistuje',
+// 				permanent: false,
+// 			},
+// 		};
+// 	}
+
+// 	return {
+// 		props: { lekce },
+// 	};
+// }
