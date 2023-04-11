5f34e8bd237f10eb58db845370ff9c33e9ef2312
Martin Šíl <martin.sil1337@gmail.com>
2023-04-11 19:11:04 +0200

    Stripe webhooky pro vytvoření a smazání předplatného

---
+++ pages/api/stripe-webhooky.js
@@ -0,0 +1,42 @@
+import { getServiceSupabase } from '@/utils/supabase';
+import { buffer } from 'micro';
+import initStripe from 'stripe';
+
+export const config = { api: { bodyParser: false } };
+
+const handler = async (req, res) => {
+	const stripe = initStripe(process.env.STRIPE_SECRET_KEY);
+	const signature = req.headers['stripe-signature'];
+	const signingSecret = process.env.STRIPE_WEBHOOK_SECRET;
+	const reqBuffer = await buffer(req);
+
+	let event;
+
+	try {
+		event = stripe.webhooks.constructEvent(reqBuffer, signature, signingSecret);
+	} catch (error) {
+		console.log(error);
+		return res.status(400).json({ error: `Webhook error: ${error.message}` });
+	}
+
+	const supabase = getServiceSupabase();
+
+	switch (event.type) {
+		case 'customer.subscription.updated':
+			await supabase
+				.from('profil')
+				.update({ odebira: true, jail: false })
+				.eq('stripe_customer_id', event.data.object.customer);
+			break;
+		case 'customer.subscription.updated':
+			await supabase
+				.from('profil')
+				.update({ odebira: false, jail: true  })
+				.eq('stripe_customer_id', event.data.object.customer);
+			break;
+	}
+
+	return res.status(200).json({ message: 'Uživatel byl upraven' });
+};
+
+export default handler;
