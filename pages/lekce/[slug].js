8095e32f463b36e059baa0b765247b499b8c9f64
Martin Šíl <martin.sil1337@gmail.com>
2023-04-15 21:26:46 +0200

    Přidání access tokenu

--- pages/lekce/[slug].js
+++ pages/lekce/[slug].js
@@ -1,12 +1,15 @@
-import { supabase } from '@/utils/supabase';
+import { getServiceSupabase, supabase } from '@/utils/supabase';
+import { JWT } from '@mux/mux-node';
 import MuxPlayer from '@mux/mux-player-react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
-export default function Lekce({ slug }) {
+export default function Lekce({ slug, accessToken }) {
 	const router = useRouter();
 	const [lekce, setLekce] = useState(null);
+
+	console.log(accessToken);
 
 	useEffect(() => {
 		const fetchLekceContent = async () => {
@@ -34,7 +37,7 @@ export default function Lekce({ slug }) {
 					</Head>
 					<h1>{lekce.nazev}</h1>
 					<div style={{ width: '40vw', height: 'auto' }}>
-						<MuxPlayer streamType="on-demand" playbackId={lekce.playback_id} />
+						<MuxPlayer streamType="on-demand" src={`https://stream.mux.com/${lekce.playback_id}.m3u8?token=${accessToken}`} />
 					</div>
 					<div dangerouslySetInnerHTML={{ __html: lekce.obsah }} />
 				</>
@@ -48,8 +51,26 @@ export default function Lekce({ slug }) {
 export async function getServerSideProps({ params }) {
 	const slug = params.slug;
 
+	const { data: lekce } = await getServiceSupabase
+		.from('lekce')
+		.select('playback_id')
+		.eq('slug', params.slug)
+		.single();
+
+	if (lekce.playback_id) {
+		const playbackId = playback_id;
+
+		let baseOptions = {
+			keyId: process.env.MUX_SECRET_KEY_ID,
+			keySecret: process.env.MUX_SECRET_BASE,
+			expiration: '7d',
+		};
+
+		const token = JWT.sign(playbackId, { ...baseOptions, type: 'video' });
+	}
+
 	return {
-		props: { slug },
+		props: { slug: slug, accessToken: token },
 	};
 }
 
